3abcfe5fb79a8102dd44dd0167cc8f56a175b0e2
Monukumar-21 <monusharma38154@gmail.com>
2025-12-25 13:43:16 +0530

    setup completed

---
+++ src/exception.py
@@ -0,0 +1,28 @@
+import sys
+
+def error_message_details(error, error_detail: sys):
+    # exc_info() returns (type, value, traceback)
+    # We only need the traceback (exc_tb)
+    _, _, exc_tb = error_detail.exc_info()
+    
+    # Extracting the file name and line number from the traceback
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, line_number, str(error)
+    )
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        # Pass the message to the parent Exception class
+        super().__init__(error_message)
+        
+        # Generate the detailed error message
+        self.error_message = error_message_details(error_message, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
