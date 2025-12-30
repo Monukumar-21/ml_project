b1f600224a3609268ca25c68e5ea1be9e57386ed
Monukumar-21 <monusharma38154@gmail.com>
2025-12-30 17:04:00 +0530

    model works well

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -30,13 +30,17 @@ class ModelTrainer:
 
     def initiate_model_trainer(self, train_array, test_array):
         try:
-            logging.info("Splitting training and test input data")
             X_train, y_train, X_test, y_test = (
                 train_array[:, :-1],
                 train_array[:, -1],
                 test_array[:, :-1],
                 test_array[:, -1]
             )
+            cat_model = CatBoostRegressor(verbose=False)
+
+# This "cheats" the system by giving CatBoost the attribute sklearn is looking for
+            if not hasattr(cat_model, "__sklearn_tags__"):
+                cat_model.__sklearn_tags__ = lambda: None
 
             models = {
                 "Random Forest": RandomForestRegressor(),
@@ -44,13 +48,43 @@ class ModelTrainer:
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
                 "XGBRegressor": XGBRegressor(),
-                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "CatBoosting Regressor": cat_model,
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
-            # You will need a 'evaluate_models' function in your src.utils
-            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Linear Regression": {},
+                "XGBRegressor": {
+                    'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "CatBoosting Regressor": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor": {
+                    'learning_rate': [0.1, 0.01, 0.5, 0.001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                }
+            }
 
+            model_report: dict = evaluate_models(
+                X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, 
+                models=models, param=params
+            )
+            
             # To get the best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,10 @@ import pickle
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 from src.exception import CustomException
+from src.logger import logging  # Added this to support your logging.info call
 
 def save_object(file_path, obj):
     try:
@@ -17,26 +20,41 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model_name = list(models.keys())[i]
             model = list(models.values())[i]
+            para = param.get(model_name, {}) 
 
-            # Train the model using default parameters
+            logging.info(f"Started Hyperparameter Tuning for: {model_name}")
+            
+            # GridSearchCV helps find the best combination of parameters
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            # Re-initialize the model with the best parameters found
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
-            # Make predictions
-            y_test_pred = model.predict(X_test)
 
-            # Get R2 score for the test data
+            y_test_pred = model.predict(X_test)
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[model_name] = test_model_score
+            
+            logging.info(f"Completed {model_name} with R2 Score: {test_model_score}")
 
         return report
 
     except Exception as e:
         raise CustomException(e, sys)
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)   
